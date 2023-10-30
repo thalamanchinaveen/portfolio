a93dae059d1694a1bc5ba2030c69d7417cfc5635
thalamanchinaveen <thalamanchinaveen@gmail.com>
2023-10-30 17:47:05 +0530

    few changes

--- src/Pages/Navbar/Navbar.jsx
+++ src/Pages/Navbar/Navbar.jsx
@@ -117,7 +117,7 @@ const Navbar = () => {
         <div className="navbar-right2">
           <div className="button-div">
             <button>
-              <a href={resume} download="resume">
+              <a href={" "} download="resume">
                 DOWNLOAD CV
               </a>
             </button>
